e2694cc74d6de2c0cbd37cf65d57a678061612ae
royas <tomroj@gmail.com>
2014-01-13 14:26:25 +0100

    Fixed problems with admin markers in username

--- qa-accept-layer.php
+++ qa-accept-layer.php
@@ -15,7 +15,7 @@
 
 			if($class == 'qa-q-view' && qa_opt('accept_plugin_show')) {
 
-				$handle = strip_tags($post['who']['data']);
+				$handle = strip_tags($post['raw']['handle']);
 				$uids = qa_handles_to_userids(array($handle));
 				$uid = $uids[$handle];
 				if(!$uid)
